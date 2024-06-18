d0daf7280c676b310a8f36732f89900a135156e8
gazservissana.zp@gmail.com <gazservissana.zp@gmail.com>
2024-06-18 15:32:49 +0300

    Initial Commit

---
+++ Docs and Photo Translator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Docs and Photo Translator
+//
+//  Created by Aleksandr on 18.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
